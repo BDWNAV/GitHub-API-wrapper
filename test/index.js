b1d494d9227988d20f0e06623c7eaa95fc1cf36b
BDWNAV <rs736611@gmail.com>
2021-07-07 17:46:35 +1000

    Fixed the test folder.

--- test/index.js
+++ test/index.js
@@ -1,2 +1,7 @@
-const github = require("github-api-wrapper");
+const github = require("@bdwnav/github-api-wrapper");
 
+github.getUser('BDWNAV').then((data) => {
+    console.log(data);
+}).catch((err) => {
+    console.log(err);
+});
