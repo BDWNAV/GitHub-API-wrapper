a8aa4e1581dbd5b1a9620688706d525b012358f4
BDWNAV <rs736611@gmail.com>
2021-07-12 17:35:20 +1000

    Made some checks with the data folder and fixed the example.

--- src/data/getRepo.js
+++ src/data/getRepo.js
@@ -1,14 +1,18 @@
 const fetch = require("node-fetch");
 
 async function getRepo(repoAuthor, repoName) {
-  const res = await fetch(`https://api.github.com/repos/${repoAuthor}/${repoName}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-
-  return res.json();
+  if(repoAuthor || repoName) {
+    const res = await fetch(`https://api.github.com/repos/${repoAuthor}/${repoName}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+  
+    return res.json();
+  } else {
+    throw new TypeError("There was no author, or name provided to get for the repo.");
+  }
 }
 
 module.exports = getRepo;
--- src/data/getUser.js
+++ src/data/getUser.js
@@ -1,13 +1,17 @@
 const fetch = require("node-fetch");
 
 async function getUser(username) {
-  const res = await fetch(`https://api.github.com/users/${username}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  return res.json();
+  if (username) {
+    const res = await fetch(`https://api.github.com/users/${username}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res.json();
+  } else {
+    throw new TypeError("There was no username provided");
+  }
 }
 
 module.exports = getUser;
